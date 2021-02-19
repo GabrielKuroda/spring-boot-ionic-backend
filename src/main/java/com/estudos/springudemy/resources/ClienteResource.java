07704c2f4bf94954f7e1f5860c9ab29b3fa9153d
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-02-19 14:06:06 -0300

    PUT,DELETE e GET para Cliente

--- src/main/java/com/estudos/springudemy/resources/ClienteResource.java
+++ src/main/java/com/estudos/springudemy/resources/ClienteResource.java
@@ -1,18 +1,19 @@
 package com.estudos.springudemy.resources;
 
-import com.estudos.springudemy.domain.Categoria;
 import com.estudos.springudemy.domain.Cliente;
-import com.estudos.springudemy.services.CategoriaService;
+import com.estudos.springudemy.dto.ClienteDTO;
 import com.estudos.springudemy.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/cliente")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
 
     @Autowired
@@ -23,5 +24,38 @@ public class ClienteResource {
         Cliente obj = clienteService.find(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable Integer id,
+                                       @Valid @RequestBody ClienteDTO objDTO){
+        Cliente obj = clienteService.fromDTO(objDTO);
+        obj.setId(id);
+        obj = clienteService.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        clienteService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ClienteDTO>> findAll(){
+        List<Cliente> list = clienteService.findAll();
+        List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
+    @RequestMapping(value = "/page",method = RequestMethod.GET)
+    public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page",defaultValue = "0") Integer page,
+                                                       @RequestParam(value = "linesPerPage",defaultValue = "24") Integer linesPerPage,
+                                                       @RequestParam(value = "orderBy",defaultValue = "nome") String orderBy,
+                                                       @RequestParam(value = "direction",defaultValue = "ASC") String direction){
+        Page<Cliente> list = clienteService.findPage(page,linesPerPage,orderBy,direction);
+        Page<ClienteDTO> listDTO = list.map(ClienteDTO::new);
+        return ResponseEntity.ok().body(listDTO);
+    }
+
 
 }
--- src/main/java/com/estudos/springudemy/services/ClienteService.java
+++ src/main/java/com/estudos/springudemy/services/ClienteService.java
@@ -1,11 +1,20 @@
 package com.estudos.springudemy.services;
 
+import com.estudos.springudemy.domain.Categoria;
 import com.estudos.springudemy.domain.Cliente;
+import com.estudos.springudemy.dto.CategoriaDTO;
+import com.estudos.springudemy.dto.ClienteDTO;
 import com.estudos.springudemy.repositories.ClienteRepository;
+import com.estudos.springudemy.services.execptions.DataIntegrityExecption;
 import com.estudos.springudemy.services.execptions.ObjectNotFoundExecption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,4 +29,36 @@ public class ClienteService {
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
     }
 
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj,obj);
+        return clienteRepository.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try{
+            clienteRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DataIntegrityExecption("Não é possivel excluir porque ha entidades relacionadas!");
+        }
+    }
+
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return clienteRepository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO dto){
+        return new Cliente(dto.getId(),dto.getNome(),dto.getEmail(),null,null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
 }
