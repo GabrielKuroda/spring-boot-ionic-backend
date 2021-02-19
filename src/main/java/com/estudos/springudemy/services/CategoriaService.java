3f3aa35ee6e594a404f5892941535e85ebbebc6d
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-02-19 11:35:24 -0300

    Paginação com Paramentros Opcionais na Requisição

--- src/main/java/com/estudos/springudemy/services/CategoriaService.java
+++ src/main/java/com/estudos/springudemy/services/CategoriaService.java
@@ -4,10 +4,11 @@ import com.estudos.springudemy.domain.Categoria;
 import com.estudos.springudemy.repositories.CategoriaRepository;
 import com.estudos.springudemy.services.execptions.DataIntegrityExecption;
 import com.estudos.springudemy.services.execptions.ObjectNotFoundExecption;
-import org.hibernate.ObjectDeletedException;
-import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -46,5 +47,10 @@ public class CategoriaService {
 
     public List<Categoria> findAll() {
         return categoriaRepository.findAll();
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return categoriaRepository.findAll(pageRequest);
     }
 }
