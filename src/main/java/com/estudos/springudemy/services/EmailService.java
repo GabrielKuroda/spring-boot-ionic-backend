d1db8eb01ea535438163c2ca9d0d9b9b0dca2a6d
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-02-23 14:33:07 -0300

    Email HTML

--- src/main/java/com/estudos/springudemy/services/EmailService.java
+++ src/main/java/com/estudos/springudemy/services/EmailService.java
@@ -3,10 +3,15 @@ package com.estudos.springudemy.services;
 import com.estudos.springudemy.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
 
     void sendEmail(SimpleMailMessage msg);
 
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+    void sendHtmlEmail(MimeMessage msg);
 }
