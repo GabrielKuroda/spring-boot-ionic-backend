143129cdf1b57776ce3df69f5197a8b0da6c3c63
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2021-02-25 09:59:45 -0300

    Refresh Token

---
+++ src/main/java/com/estudos/springudemy/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.estudos.springudemy.resources;
+
+import com.estudos.springudemy.security.JWTUtil;
+import com.estudos.springudemy.security.UserSS;
+import com.estudos.springudemy.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthResource {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+
+}
